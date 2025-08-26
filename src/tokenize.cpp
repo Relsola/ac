c9a8b3c904fec890e4ae1445ff804faa70a6c9f8
Relsola <sola001018@gmail.com>
2025-08-26 15:47:32 +0800

    Add hexadecimal, octal and binary number literals

--- src/tokenize.cpp
+++ src/tokenize.cpp
@@ -227,6 +227,28 @@ static Token *read_char_literal(char *start) {
   return tok;
 }
 
+static Token *read_int_literal(char *start) {
+  char *p = start;
+
+  int base = 10;
+  if (!strncasecmp(p, "0x", 2) && isalnum(p[2])) {
+    p += 2;
+    base = 16;
+  } else if (!strncasecmp(p, "0b", 2) && isalnum(p[2])) {
+    p += 2;
+    base = 2;
+  } else if (*p == '0') {
+    base = 8;
+  }
+
+  long val = strtoul(p, &p, base);
+  if (isalnum(*p)) error_at(p, "invalid digit");
+
+  Token *tok = new Token(TokenKind::TK_NUM, start, p);
+  tok->val = val;
+  return tok;
+}
+
 static void convert_keywords(Token *tok) {
   for (Token *t = tok; t->kind != TokenKind::TK_EOF; t = t->next)
     if (is_keyword(t)) t->kind = TokenKind::TK_KEYWORD;
@@ -277,10 +299,8 @@ static Token *tokenize(char *filename, char *p) {
 
     // Numeric literal
     if (isdigit(*p)) {
-      cur = cur->next = new Token(TokenKind::TK_NUM, p, p);
-      char *q = p;
-      cur->val = strtoul(p, &p, 10);
-      cur->len = p - q;
+      cur = cur->next = read_int_literal(p);
+      p += cur->len;
       continue;
     }
 
