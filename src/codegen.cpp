73725ef03186bf03f5b6ac6b19a8b2c724a41782
Relsola <sola001018@gmail.com>
2025-08-28 20:09:10 +0800

    Emit uninitialized global data to .bss instead of .data

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -454,11 +454,12 @@ static void emit_data(Obj *prog) {
   for (Obj *var = prog; var; var = var->next) {
     if (var->is_function) continue;
 
-    println("  .data");
     println("  .globl %s", var->name);
-    println("%s:", var->name);
 
     if (var->init_data) {
+      println("  .data");
+      println("%s:", var->name);
+
       Relocation *rel = var->rel;
       int pos = 0;
       while (pos < var->ty->size) {
@@ -470,9 +471,12 @@ static void emit_data(Obj *prog) {
           println("  .byte %d", var->init_data[pos++]);
         }
       }
-    } else {
-      println("  .zero %d", var->ty->size);
+      continue;
     }
+
+    println("  .bss");
+    println("%s:", var->name);
+    println("  .zero %d", var->ty->size);
   }
 }
 
