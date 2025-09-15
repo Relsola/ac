4b840af412b9ec85dbc8b3e3fa6ac1c286b5c419
Relsola <sola001018@gmail.com>
2025-09-15 17:08:06 +0800

    Allow array designators to initialize incomplete arrays

--- src/parse.cpp
+++ src/parse.cpp
@@ -856,20 +856,43 @@ static void designation(Token **rest, Token *tok, Initializer *init) {
   initializer2(rest, tok, init);
 }
 
+// An array length can be omitted if an array has an initializer
+// (e.g. `int x[] = {1,2,3}`). If it's omitted, count the number
+// of initializer elements.
 static int count_array_init_elements(Token *tok, Type *ty) {
-  Initializer *dummy = new_initializer(ty->base, false);
-  int i = 0;
+  bool first = true;
+  Initializer *dummy = new_initializer(ty->base, true);
 
-  for (; !consume_end(&tok, tok); i++) {
-    if (i > 0) tok = tok->skip(",");
-    initializer2(&tok, tok, dummy);
+  int i = 0, max = 0;
+
+  while (!consume_end(&tok, tok)) {
+    if (!first) tok = tok->skip(",");
+    first = false;
+
+    if (tok->equal("[")) {
+      i = const_expr(&tok, tok->next);
+      if (tok->equal("...")) i = const_expr(&tok, tok->next);
+      tok = tok->skip("]");
+      designation(&tok, tok, dummy);
+    } else {
+      initializer2(&tok, tok, dummy);
+    }
+
+    i++;
+    max = std::max(max, i);
   }
-  return i;
+  return max;
 }
 
 // array-initializer1 = "{" initializer ("," initializer)* ","? "}"
 static void array_initializer1(Token **rest, Token *tok, Initializer *init) {
   tok = tok->skip("{");
+
+  if (init->is_flexible) {
+    int len = count_array_init_elements(tok, init->ty);
+    *init = *new_initializer(Type::array_of(init->ty->base, len), false);
+  }
+
   bool first = true;
 
   if (init->is_flexible) {
